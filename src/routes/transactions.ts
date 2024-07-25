98344e76b76adc97ba97a9f4985653452610f5c8
Guilherme Gomes David <guidev001@gmail.com>
2024-07-25 20:08:47 -0300

    Utilizando cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -31,7 +31,6 @@ export async function transactionsRoutes(app: FastifyInstance) {
   })
 
   app.post('/', async (request, reply) => {
-
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
@@ -42,10 +41,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
-      amount: type === 'credit' ? amount : amount * -1
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId
     })
 
     return reply.status(201).send()
